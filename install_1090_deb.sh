030de68fcc9a738a7245f5848dfe4839d0798777
allf1090 <73718921+allf1090@users.noreply.github.com>
2020-11-02 03:33:25 +0530

    Add files via upload

---
+++ install_1090_deb.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#Bot setup V2 1090 - Debian Version
+#Adding startup support for client.
+#Making client run forever.
+
+#define static variables.
+NODE_URL="https://deb.nodesource.com/setup_10.x"                                                #V10 most stable release.
+GOLANG_URL_64="https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz"                #go 64
+GOLANG_URL_32="https://storage.googleapis.com/golang/go1.8.3.linux-386.tar.gz"                  #go 32
+
+CLIENT_SRC_URL="https://github.com/allf1090/hx2_1090/raw/main/1090_CLIENT.zip"                  #client install bundle.
+CLIENT_INSTALL_DIR="/usr/lib/"                                                                  #client install root dir
+CLIENT_INSTALL_NAME="1090_CLIENT"                                                               #client install name
+CLIENT_FULL_PATH=$CLIENT_INSTALL_DIR$CLIENT_INSTALL_NAME                                        #client root dir
+
+#define dynamic variables.
+CWD=$(pwd)                                                                                      #current working dir
+HOSTN=$(hostname)                                                                               #get hostname
+arch=$(uname -m)                                                                                #get arch
+
+echo "Updating and Installing dependencies [unzip, golang, nodejs, hping3, cron]"
+apt-get update -y > /dev/null 2>&1
+apt-get install -y curl wget unzip hping3 cron > /dev/null 2>&1
+
+#install golang
+if [ "$arch" == 'x86_64' ]
+then
+    wget $GOLANG_URL_64 > /dev/null 2>&1
+    tar -zxvf "${CWD}/go1.8.3.linux-amd64.tar.gz" -C /usr/local > /dev/null 2>&1
+    rm -r "${CWD}/go1.8.3.linux-amd64.tar.gz"
+    echo "golang  ->  done"
+else
+    wget $GOLANG_URL_32 > /dev/null 2>&1
+    tar -zxvf "${CWD}/go1.8.3.linux-386.tar.gz" -C /usr/local > /dev/null 2>&1
+    rm -r "${CWD}/go1.8.3.linux-386.tar.gz"
+    echo "golang  ->  done"
+fi
+
+#getting nodejs updated version.
+echo "Installing nodejs stable version."
+curl -sL $NODE_URL | bash - > /dev/null 2>&1
+apt-get install -y nodejs > /dev/null 2>&1
+echo "Nodejs install success"
+
+
+echo "Installing client ..."
+wget $CLIENT_SRC_URL > /dev/null 2>&1
+#rm -r $CLIENT_FULL_PATH                                                                        #removing current client
+unzip "${CLIENT_INSTALL_NAME}.zip" -d "${CLIENT_INSTALL_DIR}" 
+rm -r "${CLIENT_INSTALL_NAME}.zip"
+
+echo "adding to startup"
+#echo new cron into cron file
+(crontab -l ; echo "@reboot node ${CLIENT_FULL_PATH}/client.js") | crontab -
+#install new cron file
+echo "start up added"
+
+echo "starting up the client ..."
+node "${CLIENT_FULL_PATH}/client.js" &
+echo "client running in the background"
---
+++ install_1090_rhel.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#Bot setup V2 1090 - RedHat Version
+#Adding startup support for client.
+#Making client run forever.
+
+#define static variables.
+NODE_URL="https://rpm.nodesource.com/setup_10.x"                                                #V10 most stable release.
+GOLANG_URL_64="https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz"                #go 64
+GOLANG_URL_32="https://storage.googleapis.com/golang/go1.8.3.linux-386.tar.gz"                  #go 32
+
+CLIENT_SRC_URL="https://github.com/allf1090/hx2_1090/raw/main/1090_CLIENT.zip"                  #client install bundle.
+CLIENT_INSTALL_DIR="/usr/lib/"                                                                  #client install root dir
+CLIENT_INSTALL_NAME="1090_CLIENT"                                                               #client install name
+CLIENT_FULL_PATH=$CLIENT_INSTALL_DIR$CLIENT_INSTALL_NAME                                        #client root dir
+
+#define dynamic variables.
+CWD=$(pwd)                                                                                      #current working dir
+HOSTN=$(hostname)                                                                               #get hostname
+arch=$(uname -m)                                                                                #get arch
+
+echo "installing dependencies [unzip, golang, nodejs hping3]"
+echo y | yum update
+echo y | yum install epel-release
+echo y | yum install curl 
+echo y | yum install wget
+echo y | yum install unzip
+echo y | yum install hping3
+#echo y | yum install cron
+
+#install golang
+if [ "$arch" == 'x86_64' ]
+then
+    wget $GOLANG_URL_64
+    tar -zxvf "${CWD}/go1.8.3.linux-amd64.tar.gz" -C /usr/local
+    rm -r "${CWD}/go1.8.3.linux-amd64.tar.gz"
+    echo "golang  ->  done"
+else
+    wget $GOLANG_URL_32
+    tar -zxvf "${CWD}/go1.8.3.linux-386.tar.gz" -C /usr/local
+    rm -r "${CWD}/go1.8.3.linux-386.tar.gz"
+    echo "golang  ->  done"
+fi
+
+#getting nodejs updated version.
+echo "Installing nodejs stable version."
+curl -sL $NODE_URL | bash -
+echo y | yum install nodejs
+echo "Nodejs install success"
+
+echo "Installing client ..."
+wget $CLIENT_SRC_URL > /dev/null 2>&1
+#rm -r $CLIENT_FULL_PATH                                                                        #removing current client
+unzip "${CLIENT_INSTALL_NAME}.zip" -d "${CLIENT_INSTALL_DIR}" 
+rm -r "${CLIENT_INSTALL_NAME}.zip"
+
+echo "adding to startup"
+#echo new cron into cron file
+(crontab -l ; echo "@reboot node ${CLIENT_FULL_PATH}/client.js") | crontab -
+#install new cron file
+echo "start up added"
+
+echo "starting up the client ..."
+node "${CLIENT_FULL_PATH}/client.js" &
+echo "client running in the background"
